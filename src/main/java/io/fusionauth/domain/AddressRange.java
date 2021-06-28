ac8761cabf46715fc7e1c1b8b6d347be51647d0c
Brett Guy <brett@fusionauth.io>
2021-06-28 11:29:55 -0600

    Allow/block initial commit.

---
+++ src/main/java/io/fusionauth/domain/AddressRange.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package io.fusionauth.domain;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * @author Brett Guy
+ */
+public class AddressRange implements Buildable<AddressRange> {
+  public String endIP;
+
+  public UUID id;
+
+  public ZonedDateTime insertInstant;
+
+  public ZonedDateTime lastUpdateInstant;
+
+  public AddressRangeMode mode;
+
+  public String startIP;
+
+  public AddressRange() {
+  }
+
+  public AddressRange(String startIP, String endIP, AddressRangeMode mode) {
+    this.startIP = startIP;
+    this.endIP = endIP;
+    this.mode = mode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AddressRange that = (AddressRange) o;
+    return Objects.equals(endIP, that.endIP) &&
+           Objects.equals(id, that.id) &&
+           Objects.equals(insertInstant, that.insertInstant) &&
+           Objects.equals(lastUpdateInstant, that.lastUpdateInstant) &&
+           Objects.equals(mode, that.mode) &&
+           Objects.equals(startIP, that.startIP);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(endIP, id, insertInstant, lastUpdateInstant, mode, startIP);
+  }
+}
